c79b089d5966849c03b1be9facb9db7d01253b66
Sarah Roberts <sonoran.sarah@gmail.com>
2025-08-29 20:05:19 -0700

    CORE-2072: updated dependencies

--- project.clj
+++ project.clj
@@ -16,7 +16,7 @@
   :manifest {"Git-Ref" ~(git-ref)}
   :uberjar-name "dewey-standalone.jar"
   :main ^:skip-aot dewey.core
-  :dependencies [[org.clojure/clojure "1.11.3"]
+  :dependencies [[org.clojure/clojure "1.12.2"]
                  [org.clojure/tools.cli "1.1.230"]
                  [org.clojure/test.check "1.1.1"]
                  [cheshire "5.13.0"
@@ -25,17 +25,17 @@
                                 [com.fasterxml.jackson.core/jackson-annotations]
                                 [com.fasterxml.jackson.core/jackson-databind]
                                 [com.fasterxml.jackson.core/jackson-core]]]
-                 [com.novemberain/langohr "5.4.0" :exclusions [org.slf4j/slf4j-api]]
+                 [com.novemberain/langohr "5.6.0" :exclusions [org.slf4j/slf4j-api]]
                  [liberator "0.15.3"]
                  [compojure "1.7.1"]
-                 [ring "1.12.2"]
+                 [ring "1.14.2"]
                  [slingshot "0.12.2"]
-                 [org.cyverse/clj-jargon "3.1.1"
+                 [org.cyverse/clj-jargon "3.1.4"
                    :exclusions [[org.slf4j/slf4j-log4j12]
                                 [log4j]]]
-                 [org.cyverse/clojure-commons "3.0.9"]
+                 [org.cyverse/clojure-commons "3.0.11"]
                  [org.cyverse/common-cli "2.8.2"]
-                 [org.cyverse/service-logging "2.8.4"]
+                 [org.cyverse/service-logging "2.8.5"]
                  [org.cyverse/event-messages "0.0.1"]
                  [me.raynes/fs "1.4.6"]
                  [cc.qbits/spandex "0.8.2"]
